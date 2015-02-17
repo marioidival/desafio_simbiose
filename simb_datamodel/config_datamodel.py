53aaa822752fc0ed640b1f54d1b4466279887055
Mario Idival <marioidival@gmail.com>
2015-02-17 01:55:00 -0300

    create keyspace when running first time

--- simb_datamodel/config_datamodel.py
+++ simb_datamodel/config_datamodel.py
@@ -1,20 +1,25 @@
 """
     File to config Cassandra and ElasticSearch data models
 """
-from cqlengine.management import sync_table
+from cqlengine.management import sync_table, create_keyspace
 from cqlengine.connection import setup
 from elasticsearch_dsl.connections import connections
 
-from simb_cassandra import LoanCassandra
-from simb_elasticsearch import LoanES
+from simb_cassandra import Loan as LoanCassandra
+from simb_elasticsearch import Loan as LoanES
+
 
 def config_cassandra():
     """
     Method to startup cqlengine data model
     """
     keyspace_name = "simbiose_test"
+
     # setup cassandra and set default keyspaces to models
     setup(['localhost'], keyspace_name)
+
+    create_keyspace(keyspace_name, replication_factor=1,
+                    strategy_class="SimpleStrategy")
 
     # Create tables to keyspaces
     sync_table(LoanCassandra)
