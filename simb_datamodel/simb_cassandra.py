de47f151675f9530e022d9b308b1d270d7bc477b
Mario Idival <marioidival@gmail.com>
2015-02-17 01:56:49 -0300

    add types_cassandra dict and improvement docstrings

--- simb_datamodel/simb_cassandra.py
+++ simb_datamodel/simb_cassandra.py
@@ -1,10 +1,39 @@
 """ Data Model of cassandra """
 import uuid
+import decimal
+import datetime
+from collections import OrderedDict
 from cqlengine import columns, Model
 
 
-class LoanCassandra(Model):
+class Loan(Model):
+    """Data Model to Loan table in Cassandra"""
 
     id = columns.UUID(primary_key=True, default=uuid.uuid4)
     person = columns.Text(required=True)
     loan_value = columns.Decimal(required=True)
+
+GENERATOR = type((i for i in range(1, 3)))
+
+# Dict with key as python type and values as cql literal type
+types_cassandra = {
+    None: "NULL",
+    bool: "boolean",
+    float: "double",
+    int: "int",
+    long: "varint",
+    decimal.Decimal: "decimal",
+    str: "varchar",
+    unicode: "ascii",
+    datetime.date: "timestamp",
+    datetime.datetime: "timestamp",
+    list: "list",
+    tuple: "list",
+    GENERATOR: "list",
+    set: "set",
+    frozenset: "set",
+    dict: "map",
+    OrderedDict: "map",
+    uuid.UUID: "timeuuid",
+    uuid.UUID: "uuid",
+}
--- simb_datamodel/simb_elasticsearch.py
+++ simb_datamodel/simb_elasticsearch.py
@@ -3,7 +3,8 @@ import uuid
 from elasticsearch_dsl import DocType, String, Double
 
 
-class LoanES(DocType):
+class Loan(DocType):
+    """Data Model to Loan Doc Type in ElasticSearch"""
 
     person = String()
     loan_value = Double()
@@ -12,5 +13,6 @@ class LoanES(DocType):
         index = "simbiose_test"
 
     def save(self, **kwargs):
+        """Define id as uuid4"""
         self.id = str(uuid.uuid4())
         return super(LoanES, self).save(**kwargs)
